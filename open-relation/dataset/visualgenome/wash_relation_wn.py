28c01eafa80e935e523646144c64cec7bf0fecf2
sx14 <1059363093@qq.com>
2018-10-09 14:26:16 +0800

    ..

--- open-relation/dataset/visualgenome/wash_relation_wn.py
+++ open-relation/dataset/visualgenome/wash_relation_wn.py
@@ -10,7 +10,7 @@ def wash_relation_wn(relation_label2wn_path):
     legal_pos_list = ['IN', 'JJ', 'JJR', 'JJS', 'RP', 'TO',     # 介词，形容词，比较级，最高级，虚词，to
                       'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']  # 动词，过去式，现在分词，过去分词，现在非三单，现在三单
     with open(relation_label2wn_path, 'r') as label2wn_file:
-        label2wn = json.load(label2wn_file, 'r')
+        label2wn = json.load(label2wn_file)
     lemmatizer = WordNetLemmatizer()
     wn_stub = 'relation.x.01'
     for label in label2wn.keys():
