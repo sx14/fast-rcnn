cfe46c7524a4f0284fa27c5c803062d22b73db49
sx14 <1059363093@qq.com>
2018-10-11 20:10:04 +0800

    add hidden layer for nn

--- open-relation/model/model.py
+++ open-relation/model/model.py
@@ -5,11 +5,14 @@ from torch import nn
 class HypernymVisual(nn.Module):
     def __init__(self, visual_feature_dimension, embedding_dimension):
         super(HypernymVisual, self).__init__()
-        self.embedding = nn.Linear(visual_feature_dimension, embedding_dimension)
+        hidden_layer_unit_num = 1500
+        self.hidden = nn.Linear(visual_feature_dimension, hidden_layer_unit_num)
+        self.embedding = nn.Linear(hidden_layer_unit_num, embedding_dimension)
         self.activate = nn.ReLU()
 
     def forward(self, vf, wf):
-        vf_embedding = self.embedding.forward(vf)
+        hidden_out = self.hidden.forward(vf)
+        vf_embedding = self.embedding.forward(hidden_out)
         sub = wf - vf_embedding
         act = self.activate.forward(sub)
         act_pow = act * act
