c275ce4b501e0f9ac821f68ae069f17fa66666ab
sx14 <1059363093@qq.com>
2018-10-11 16:17:31 +0800

    ..

--- open-relation/dataset/visualgenome/wash_relation_wn.py
+++ open-relation/dataset/visualgenome/wash_relation_wn.py
@@ -33,10 +33,6 @@ def wash_relation_label(org_anno_root, output_anno_root):
                 if pos in legal_pos_list:   # legal predicate words
                     if pos.startswith('VB'):
                         org_word = lemmatizer.lemmatize(word, pos='v')  # reshape word to original
-                    elif pos.startswith('JJ'):
-                        org_word = lemmatizer.lemmatize(word, pos='a')  # reshape word to original
-                    elif pos.startswith('R'):
-                        org_word = lemmatizer.lemmatize(word, pos='r')  # reshape word to original
                     else:
                         org_word = word
                     new_label_words.append(org_word)
