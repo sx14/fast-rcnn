42077878e56d745d170efb2ef5cd20f81429484a
sx14 <1059363093@qq.com>
2018-10-25 14:16:15 +0800

    ..

--- open-relation/wordnet-embedding/object/preprocessObjectWordnet.py
+++ open-relation/wordnet-embedding/object/preprocessObjectWordnet.py
@@ -1,5 +1,4 @@
 import os
-import copy
 import json
 import numpy as np
 from nltk.corpus import wordnet as wn
@@ -8,41 +7,38 @@ import path_config
 FOR_VS = True
 if FOR_VS:
     vs2wn_path = os.path.join(path_config.OBJECT_SAVE_ROOT, 'label2wn.json')
-    # all_nouns: wordnet nodes mapped by visual genome object labels
     with open(vs2wn_path, 'r') as vs2wn_file:
         vs2wn = json.load(vs2wn_file)
     vs_wns = vs2wn.values()
-    all_nouns = set()
+    wn_nouns = set()
     for wn_names in vs_wns:
         for wn_name in wn_names:
             wn_synset = wn.synset(wn_name)
             for p in wn_synset.hypernym_paths():
                 for w in p:
-                    all_nouns.add(w)
-    all_nouns = list(all_nouns)
+                    wn_nouns.add(w)
+    wn_nouns = list(wn_nouns)
 else:
-    all_nouns = list(wn.all_synsets('n'))
+    wn_nouns = list(wn.all_synsets('n'))
 
 # get mapping of synset id to index
 id2index = {}
-for i in range(len(all_nouns)):
-    id2index[all_nouns[i].name()] = i
+for i in range(len(wn_nouns)):
+    id2index[wn_nouns[i].name()] = i
 
-# get hypernym relations
+# get wordnet part hypernym relations
 hypernyms = []
-for synset in all_nouns:
+for synset in wn_nouns:
     for h in synset.hypernyms() + synset.instance_hypernyms():
         hypernyms.append([id2index[synset.name()], id2index[h.name()]])
 if FOR_VS:
     # ==== append Visual Genome object classes ====
-    stub_node = wn.synset('entity.n.01')
+    vs_nouns = []
     next_id2index_id = len(id2index)
-    for vs_object in vs2wn:
-        temp_node = copy.deepcopy(stub_node)
-        temp_node._name = vs_object
-        all_nouns.append(temp_node)
-        id2index[vs_object] = next_id2index_id
-        wns = vs2wn[vs_object]
+    for vs_label in vs2wn:
+        vs_nouns.append(vs_label)
+        id2index[vs_label] = next_id2index_id
+        wns = vs2wn[vs_label]
         for h in wns:
             hypernyms.append([next_id2index_id, id2index[h]])
         next_id2index_id = next_id2index_id + 1
@@ -75,7 +71,9 @@ f.close()
 
 
 # save list of synset names
-names = map(lambda s: s.name(), all_nouns)
+names = map(lambda s: s.name(), wn_nouns)
+if FOR_VS:
+    names = names + vs_nouns
 if FOR_VS:
     wn2index_save_path = os.path.join(path_config.OBJECT_SAVE_ROOT, 'wn2index.json')
     json.dump(id2index, open(wn2index_save_path, 'w'))
