14b1dc134aa69d63a785aae78e5221ae43829f79
sx14 <1059363093@qq.com>
2018-10-30 14:01:43 +0800

    config files

--- open-relation/wordnet-embedding/object/path_config_sample.py
+++ open-relation/wordnet-embedding/object/path_config_sample.py
@@ -1,5 +1,21 @@
 import os
 
-VS_ROOT = ''
+VS_ROOT = '/media/sunx/Data/dataset/visual genome'
+PASCAL_ROOT = '/media/sunx/Data/dataset/voc2007/VOCdevkit/VOC2007'
 
-SAVE_ROOT = os.path.join(VS_ROOT, 'feature', 'prepare')
+vs_config = {
+    'object_prepare_root': os.path.join(VS_ROOT, 'feature', 'object', 'prepare'),
+    'hypernym_data_path': 'vs_dataset/wordnet_with_vs.h5',
+    'synset_names_path': 'vs_dataset/synset_names_with_vs.json'
+}
+
+pascal_config = {
+    'object_prepare_root': os.path.join(PASCAL_ROOT, 'feature', 'prepare'),
+    'hypernym_data_path': 'pascal_dataset/wordnet_with_pascal.h5',
+    'synset_names_path': 'pascal_dataset/synset_names_with_pascal.json'
+}
+
+config = {
+    'vs': vs_config,
+    'pascal': pascal_config
+}
