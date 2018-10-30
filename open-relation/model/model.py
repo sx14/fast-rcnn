8c7c321299b82082a51be8b273b7f879107d9f63
sx14 <1059363093@qq.com>
2018-10-30 20:35:35 +0800

    pairwise linear model

--- open-relation/model/model.py
+++ open-relation/model/model.py
@@ -8,33 +8,18 @@ class HypernymVisual(nn.Module):
         self.embedding = nn.Linear(visual_feature_dimension, embedding_dimension)
         self.activate = nn.ReLU()
 
-    def forward(self, vf, wf):
+    def forward(self, vf, p_wfs, n_wfs):
         vf_embedding = self.embedding.forward(vf)
-        sub = wf - vf_embedding
-        act = self.activate.forward(sub)
-        act_pow = act * act
-        e = act_pow.sum(1)
-        return e.view(len(e.data), 1)
+        p_sub = p_wfs - vf_embedding
+        p_act = self.activate.forward(p_sub)
+        p_act_pow = p_act * p_act
+        p_e = p_act_pow.sum(1)
 
-
-class HypernymVisual1(nn.Module):
-    def __init__(self, visual_feature_dimension, embedding_dimension):
-        super(HypernymVisual1, self).__init__()
-        hidden_layer_unit_num = 1500
-        self.hidden = nn.Linear(visual_feature_dimension, hidden_layer_unit_num)
-        self.embedding = nn.Linear(hidden_layer_unit_num, embedding_dimension)
-        self.activate = nn.ReLU()
-
-    def forward(self, vf, wf):
-        # preserve values of vf positive
-        vf = self.activate(vf)
-        hidden_out = self.hidden.forward(vf)
-        vf_embedding = self.embedding.forward(hidden_out)
-        sub = wf - vf_embedding
-        act = self.activate.forward(sub)
-        act_pow = act * act
-        e = act_pow.sum(1)
-        return e.view(len(e.data), 1)
+        n_sub = n_wfs - vf_embedding
+        n_act = self.activate.forward(n_sub)
+        n_act_pow = n_act * n_act
+        n_e = n_act_pow.sum(1)
+        return p_e.view(len(p_e.data), 1), n_e.view(len(n_e.data), 1)
 
 
 class HypernymVisual3(nn.Module):
@@ -60,6 +45,29 @@ class HypernymVisual3(nn.Module):
         n_act_pow = n_act * n_act
         n_e = n_act_pow.sum(1)
         return p_e.view(len(p_e.data), 1), n_e.view(len(n_e.data), 1)
+
+
+class HypernymVisual1(nn.Module):
+    def __init__(self, visual_feature_dimension, embedding_dimension):
+        super(HypernymVisual1, self).__init__()
+        hidden_layer_unit_num = 1500
+        self.hidden = nn.Linear(visual_feature_dimension, hidden_layer_unit_num)
+        self.embedding = nn.Linear(hidden_layer_unit_num, embedding_dimension)
+        self.activate = nn.ReLU()
+
+    def forward(self, vf, wf):
+        # preserve values of vf positive
+        vf = self.activate(vf)
+        hidden_out = self.hidden.forward(vf)
+        vf_embedding = self.embedding.forward(hidden_out)
+        sub = wf - vf_embedding
+        act = self.activate.forward(sub)
+        act_pow = act * act
+        e = act_pow.sum(1)
+        return e.view(len(e.data), 1)
+
+
+
 
 
 class HypernymVisual2(nn.Module):
