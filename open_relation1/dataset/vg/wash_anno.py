f5ac3bcac173908c7cb407918b1d807d76b0d7ca
sx14 <1059363093@qq.com>
2018-12-04 09:37:43 +0800

    wash anno: use objects in relationships

--- open_relation1/dataset/vg/wash_anno.py
+++ open_relation1/dataset/vg/wash_anno.py
@@ -3,6 +3,56 @@ import copy
 import json
 from open_relation1.data_config import vg_config
 
+def clean_anno1(dirty_anno_path, clean_anno_path):
+    dirty_anno = json.load(open(dirty_anno_path, 'r'))
+    clean_anno = copy.deepcopy(dirty_anno)
+    cleaned_objs = []
+    cleaned_rlts = []
+    # clean relationships
+    objIds = set()
+    rlt_dict = dict()
+    rlt_annos = dirty_anno['relationships']
+    for rlt in rlt_annos:
+        # use objects in relationships
+        # clean objects
+        objs = [rlt['subject'], rlt['object']]
+        for o in objs:
+            if o['object_id'] not in objIds and len(o['synsets']) > 0:
+                objIds.add(o['object_id'])
+                cleaned_objs.append(o)
+        sid = rlt['subject']['object_id']
+        oid = rlt['object']['object_id']
+        pre_syn = rlt['synsets']
+        if sid not in objIds:
+            # subject has no wn synset
+            continue
+        if oid not in objIds:
+            # object has no wn synset
+            continue
+        if len(pre_syn) == 0:
+            # predicate has no wn synset
+            continue
+        predicate = rlt['predicate']
+        if sid in rlt_dict:
+            obj_rlts = rlt_dict[sid]
+            if oid in obj_rlts:
+                rlts = obj_rlts[oid]
+            else:
+                rlts = list()
+            # s -> o
+            if predicate not in rlts:
+                rlts.append(predicate)
+                cleaned_rlts.append(rlt)
+            obj_rlts[oid] = rlts
+        else:
+            obj_rlts = dict()
+            obj_rlts[oid] = [predicate]
+            rlt_dict[sid] = obj_rlts
+            cleaned_rlts.append(rlt)
+    clean_anno['objects'] = cleaned_objs
+    clean_anno['relationships'] = cleaned_rlts
+    json.dump(clean_anno, open(clean_anno_path, 'w'), indent=4)
+
 
 def clean_anno(dirty_anno_path, clean_anno_path):
     dirty_anno = json.load(open(dirty_anno_path, 'r'))
